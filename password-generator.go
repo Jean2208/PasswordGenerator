8cb4989d8001e787527ac6a3572d3f8a1ddd7ed9
Jean Penso <jeanpaulpenso01@live.com>
2023-01-29 17:36:57 -0600

    Commit

--- password-generator.go
+++ password-generator.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/hex"
 	"math/rand"
 	"time"
+
+	"golang.org/x/crypto/argon2"
 )
 
 const ( //declaring variabales inside const makes sure these variables are not altered during program execution
@@ -11,21 +14,43 @@ const ( //declaring variabales inside const makes sure these variables are not a
 	numberBytes    = "0123456789"                                           //numbers
 	allBytes       = letterBytes + specialBytes + numberBytes               //all bytes together
 	passwordLength = 12                                                     //password length
+	saltLength     = 16
+	memory         = 32 * 1024
+	threads        = 4
+	times          = 3
 )
 
-func init() { //The function init runs before the main function
-	rand.Seed(time.Now().UnixNano()) //The purpose of seeding a random number generator is to ensure that passwords generated are different each time the program is run
+func init() {
+	rand.Seed(time.Now().UnixNano())
 }
 
 func generatePassword() string {
-	result := make([]byte, passwordLength) //makes a slice
-	for i := range result {                //for i g in range 12 in this case
-		result[i] = allBytes[rand.Intn(len(allBytes))] //Assigning the values of the slice random values from the variable allBytes
+	result := make([]byte, passwordLength)
+	for i := range result {
+		result[i] = allBytes[rand.Intn(len(allBytes))]
 	}
-	return string(result) //returns the password to the main function
+	return string(result)
+}
+
+func hashingSalting(password string) []byte {
+	password_to_hash := []byte(password)
+	salt := make([]byte, saltLength)
+	_, err := rand.Read(salt)
+
+	if err != nil {
+		panic(err)
+	}
+
+	salted_hash := argon2.IDKey(password_to_hash, salt, times, memory, threads, 32)
+
+	return salted_hash
+
 }
 
 func main() {
-	password := generatePassword() //calls the function to generate a password
-	println(password)              //prints the password
+	password := generatePassword()
+	salted_hash := hex.EncodeToString(hashingSalting(password))
+	println(password)
+	println(salted_hash)
+
 }
